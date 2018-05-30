8c8a6c4ed7192643222081f492ad0a0ddf134e58
RenegadeB5 <rocketmanB5@gmail.com>
2018-05-30 18:58:30 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,10 +22,6 @@ con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
   /*Create a database named "mydb":*/
-  con.query("CREATE DATABASE mydb", function (err, result) {
-    if (err) throw err;
-    console.log("Database created");
-  });
 });
 
 client.on('ready', () => {
