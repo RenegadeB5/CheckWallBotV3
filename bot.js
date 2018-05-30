663a6af900a8e01ec370783da5745cb5f64c32fe
RenegadeB5 <rocketmanB5@gmail.com>
2018-05-30 12:56:08 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,7 +33,7 @@ client.on('ready', () => {
                        tag = '@here'
                  }
             function notify() {                      
-                      message = tag + 'The walls have not been checked in' + minutes + 'minutes.'
+                      message = tag + " " + 'The walls have not been checked in' + " " + minutes + " " + 'minutes.'
                       minutes += 1
                       NOTIFY_CHANNEL.sendMessage (message)
             }
