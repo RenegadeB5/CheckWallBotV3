75c0f27a5d1daaae32929afad40a3f6bde49d396
RenegadeB5 <rocketmanB5@gmail.com>
2018-08-21 19:27:36 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,9 @@ client.on('ready', () => {
       console.log('successfully Logged In As Wall Check Bot!');
       NOTIFY_CHANNEL = client.channels.find("name", "checkwall");
       
-      function () {
+      start();
+      
+      function start() {
             console.log('restarted');
             let minutes = 0
             var inter1 = setInterval(timeto, 60000);
@@ -57,19 +59,21 @@ client.on('message', message => {
     lastSender = message.guild.lastSender = message.author    
     NOTIFY_CHANNEL.sendMessage(lastSender + " " + 'has cleared the walls.')
     
-      function () {
+      function (stop) {
             console.log('cleared');
             clearInterval(inter1);
             clearInterval(inter2);
             clearInterval(inter3);
       }
-      function () {
+      function (start) {
             console.log('restarted');
             let minutes = 0
             var inter1 = setInterval(timeto, 60000);
             var inter2 = setInterval(counter, 60000);
             var inter3 = setInterval(notify, 1000);
       }
+      stop();
+      setTimeout(start, 100);
     
        
   }   
