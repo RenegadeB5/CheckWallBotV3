e9149db59055345f75baa3763a179278a6704937
RenegadeB5 <rocketmanB5@gmail.com>
2018-08-23 19:45:45 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -97,7 +97,6 @@ client.on('message', message => {
             NOTIFY_CHANNEL.sendMessage(lastSender + " " + 'has cleared the walls and has gained 1 point.')
             var findID = message.member.roles.map(r => r.name);
             const found1 = findID.filter(word => word.length > 26);
-            var words = found1.split(' ');
             console.log(found1);
         }
         else {
