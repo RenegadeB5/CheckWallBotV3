3cf9d2378748539943ee30399ceb645e007fdd72
RenegadeB5 <rocketmanB5@gmail.com>
2018-08-24 23:34:04 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -157,7 +157,7 @@ client.on ('message', message => {
         for (var i in lbF) {
               let lbmessage = lbF.points + lbF.nick2
               NOTIFY_CHANNEL.sendMessage(lbmessage);
-              console.log(lbF[0].points[0]);
+              console.log(lbF[0].points);
         }
         }
 });
